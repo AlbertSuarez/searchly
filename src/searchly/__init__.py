e4e9e26e10c467c0233ceef89d717beb447c43a8
Albert Suarez <alsumo95@gmail.com>
2019-12-14 11:49:28 +0100

    Updated DB ports

--- src/searchly/__init__.py
+++ src/searchly/__init__.py
@@ -3,7 +3,9 @@ DB_USER = 'searchly'
 DB_PASSWORD = 'searchly1234'
 DB_DB = 'searchly'
 DB_HOST = 'searchly_db'
-DB_PORT = 8089
+# DB_HOST = 'localhost'
+DB_PORT = 5432
+# DB_PORT = 8089
 
 # API settings
 API_SONG_SEARCH_LIMIT = 100
