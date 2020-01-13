b8df57fba67c08ef31165ddcf15319491434e4b7
Albert Suarez <alsumo95@gmail.com>
2020-01-13 19:32:22 +0100

    Added try/except blocks for similarity endpoint

--- src/searchly/api/v1/controllers/similarity.py
+++ src/searchly/api/v1/controllers/similarity.py
@@ -1,15 +1,20 @@
 from flask import request
 
 from src.searchly.api.v1.services import response
-from src.searchly.helper import searcher
+from src.searchly.helper import searcher, log
 
 
 def by_song():
-    song_id = request.args.get('song_id')
-    if not song_id:
-        return response.make(error=True, message='`song_id` missed as a query parameter.')
-    features = searcher.extract_features(song_id)
-    if features is None:
-        return response.make(error=True, message='Song not found.')
-    results = searcher.search(features, song_id=song_id)
-    return response.make(error=False, response=dict(similarity_list=results))
+    try:
+        song_id = request.args.get('song_id')
+        if not song_id:
+            return response.make(error=True, message='`song_id` missed as a query parameter.')
+        features = searcher.extract_features(song_id)
+        if features is None:
+            return response.make(error=True, message='Song not found.')
+        results = searcher.search(features, song_id=song_id)
+        return response.make(error=False, response=dict(similarity_list=results))
+    except Exception as e:
+        log.error(f'Unexpected error: [{e}]')
+        log.exception(e)
+        return response.make(error=True, message='Unexpected error.')
