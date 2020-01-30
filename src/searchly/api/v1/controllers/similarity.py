ae71fc1b1b385b142a181d6de3eaf44bca052a23
Albert Suarez <alsumo95@gmail.com>
2020-01-30 17:10:29 +0100

    Implemented cache to similarity/by_song

--- src/searchly/api/v1/controllers/similarity.py
+++ src/searchly/api/v1/controllers/similarity.py
@@ -1,7 +1,7 @@
 from flask import request
 
 from src.searchly.api.v1.services import response
-from src.searchly.helper import searcher, log
+from src.searchly.helper import searcher, log, cache
 
 
 def by_song():
@@ -10,14 +10,24 @@ def by_song():
     :return: JSON response.
     """
     try:
+        # Parameters retrieving
         song_id = request.args.get('song_id')
         if not song_id:
             return response.make(error=True, message='`song_id` missed as a query parameter.')
+        # Cache processing
+        method = by_song.__name__
+        key = '{}'.format(song_id)
+        results_cached = cache.get(method, key)
+        if results_cached is not None:
+            return response.make(response=results_cached, cached=True)
+        # Feature extraction
         features = searcher.extract_features_from_song(song_id)
         if features is None:
-            return response.make(error=True, message='Song not found.')
+            return response.make(error=True, message='Song not found.', method=method, key=key)
+        # Searching
         results = searcher.search(features, song_id=song_id)
-        return response.make(error=False, response=dict(similarity_list=results))
+        # Return results and refresh cache
+        return response.make(error=False, response=dict(similarity_list=results), method=method, key=key)
     except Exception as e:
         log.error(f'Unexpected error: [{e}]')
         log.exception(e)
@@ -30,6 +40,7 @@ def by_content():
     :return: JSON response.
     """
     try:
+        # Parameters retrieving
         request_json = request.json
         content = response.get('content', request_json)
         if not content:
