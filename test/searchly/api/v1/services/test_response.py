05c2fccdf2f633da1b525772a4c40ff90faaf01a
Albert Suarez <alsumo95@gmail.com>
2020-01-30 17:30:58 +0100

    Removed two unneeded tests now that we have cache in place

--- test/searchly/api/v1/services/test_response.py
+++ test/searchly/api/v1/services/test_response.py
@@ -13,27 +13,6 @@ class ResponseTest(unittest.TestCase):
         self.value = 'value'
         self.dict = {self.key: self.value}
 
-    def test_make_error(self):
-        json_response, status_code = response.make(error=True, message=self.message)
-        self.assertIs(type(json_response), dict)
-        self.assertIs(type(status_code), int)
-        self.assertIn('error', json_response)
-        self.assertIn('message', json_response)
-        self.assertEqual(json_response['error'], True)
-        self.assertEqual(json_response['message'], self.message)
-        self.assertEqual(status_code, 200)
-
-    def test_make_success(self):
-        json_response, status_code = response.make(error=False, response=self.dict)
-        self.assertIs(type(json_response), dict)
-        self.assertIs(type(status_code), int)
-        self.assertIn('error', json_response)
-        self.assertIn('response', json_response)
-        self.assertIn(self.key, json_response['response'])
-        self.assertEqual(json_response['error'], False)
-        self.assertEqual(json_response['response'][self.key], self.value)
-        self.assertEqual(status_code, 200)
-
     def test_get_found(self):
         value = response.get(self.key, self.dict)
         self.assertEqual(value, self.value)
